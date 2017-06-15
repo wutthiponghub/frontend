1af02c879b919916c27d24d772ad6f03596fbf75
Wutthipong Chinnasri <wutthipong.c@rsu.ac.th>
2017-06-15 20:11:07 +0700

    ShowMenuWithFilter

--- menu/menu.js
+++ menu/menu.js
@@ -1,78 +1,72 @@
-app.controller('menuController', function($scope, FBoperation) {
+app.controller('menuController', function($scope, FBoperation, DBoperation) {
+
+
+
+    $scope.selectType = function(tmp) {
+        console.log(tmp);
+        $scope.type = null;
+        if (tmp == 'drink') {
+            $scope.type = $scope.drink;
+            $scope.selectedtype = 'drink';
+        } else if (tmp == 'bakery') {
+            $scope.type = $scope.bakery;
+            $scope.selectedtype = 'bakery';
+        }
+        $scope.selectedsubtype = '';
+        console.log($scope.type);
+    };
+
+    $scope.selectSubType = function(tmp) {
+        console.log(tmp);
+        $scope.selectedsubtype = tmp;
+        console.log($scope.type);
+    };
+
+    DBoperation.getData('bakery').then(
+        function(data) {
+            console.log(data);
+            $scope.bakery = data;
+        },
+        function(error) {
+            console.log(error);
+        }
+    );
+    DBoperation.getData('bakerytype').then(
+        function(data) {
+            console.log(data);
+            $scope.bakerytype = data;
+        },
+        function(error) {
+            console.log(error);
+        }
+    );
 
     $scope.drinktype = FBoperation.getData('drinktype');
+    $scope.drinktype.$loaded()
+        .then(function() {
 
+        });
     $scope.drink = FBoperation.getData('drink');
     $scope.drink.$loaded()
         .then(function() {
-            $scope.load();
+
         });
 
-    $scope.drink.$watch(function(a) {
-        console.log(a);
-        if ($scope.myTable) {
-            $scope.myTable.destroy();
-            $scope.load();
-        }
-    });
+    // $scope.drink.$watch(function(a) {
+    //     console.log(a);
+    //     if ($scope.myTable) {
+    //         $scope.myTable.destroy();
+    //         $scope.load();
+    //     }
+    // });
 
 
     $scope.resetForm = function() {
-        $scope.show = false;
-        $scope.edit = false;
-        $scope.action = '';
-        $scope.tmp = {};
-        $scope.tmp.name = '';
-        $scope.tmp.price = '';
-        $scope.tmp.type = '';
+
     };
 
-    $scope.load = function() {
-
-        function filterColumn(i) {
-            $('#drinktable').DataTable().column(i).search(
-                $('#col' + i + '_filter').val()
-            ).draw();
-        }
-
-        $(document).ready(function() {
-            $scope.myTable = $('#drinktable').DataTable({
-                "paging": true,
-                "ordering": true,
-                "info": true
-            });
-            $('input.column_filter').on('keyup click', function() {
-                filterColumn($(this).attr('data-column'));
-            });
-            $('select.column_filter').on('keyup click', function() {
-                filterColumn($(this).attr('data-column'));
-            });
-        });
-    }
 
 
-
-    $scope.operation = function(tmp, action) {
-        if (action == "add") {
-            $scope.drink.$add(tmp);
-            $scope.resetForm();
-        }
-
-        if (action == "edit") {
-            $scope.resetForm();
-            console.log(tmp);
-            $scope.tmp = tmp;
-            $scope.tmp.price = parseFloat($scope.tmp.price);
-            $scope.show = true;
-            $scope.edit = true;
-            $scope.action = 'update';
-        }
-        if (action == "update") {
-            $scope.drink.$save(tmp);
-            $scope.resetForm();
-        }
-
-    }
 
 
 });
